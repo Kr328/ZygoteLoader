6707c02dbb46d660c9490b4dafccd701191d0fcb
Kr328 <kr328app@outlook.com>
2022-02-01 20:54:50 +0800

    Fix: fix restore permission

--- runtime/src/main/assets/customize.d/99-restore-permissions.sh
+++ runtime/src/main/assets/customize.d/99-restore-permissions.sh
@@ -2,4 +2,5 @@
 
 ui_print "- Restore permissions"
 set_perm_recursive "$MODPATH"          0    0    0755 0644
-set_perm_recursive "$MODULE_DATA_PATH" 1000 1000 0700 0600
+set_perm           "$DATA_PATH"        1000 1000 0700 0600 u:object_r:system_data_file:s0
+set_perm           "$MODULE_DATA_PATH" 1000 1000 0700 0600 u:object_r:system_data_file:s0
