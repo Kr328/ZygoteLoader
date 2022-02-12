cebf5ee2f74884b2e96e78f2d8c7d5a49ccc0eb8
Kr328 <kr328app@outlook.com>
2022-02-12 12:39:25 +0800

    Improve: improve data directory creation

--- runtime/src/main/assets/customize.d/40-initialize-data-directory.sh
+++ runtime/src/main/assets/customize.d/40-initialize-data-directory.sh
@@ -1,19 +1,23 @@
 # Generate ZygoteLoader data directory
 
-if [ ! -f "/data/adb/zloader/data-root-directory" ]; then
+DATA_PATH_FILE="/data/adb/zloader/data-root-directory"
+
+if [ ! -f "$DATA_PATH_FILE" ]; then
   ui_print "- Initialize data directory"
 
-  mkdir -p "/data/adb/zloader"
+  mkdir -p "$(dirname "$DATA_PATH_FILE")"
 
   RANDOM_SUFFIX="const"
 
-  [ -e "/dev/urandom" ] && RANDOM_SUFFIX=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 16)
+  [ -e "/dev/urandom" ] && RANDOM_SUFFIX="$(tr -dc A-Za-z0-9 < "/dev/urandom" | head -c 16)"
 
-  echo -n "/data/system/zloader-$RANDOM_SUFFIX" > "/data/adb/zloader/data-root-directory"
+  echo -n "/data/system/zloader-$RANDOM_SUFFIX" > "$DATA_PATH_FILE"
 fi
 
-DATA_PATH=$(cat "/data/adb/zloader/data-root-directory")
+DATA_PATH="$(cat "$DATA_PATH_FILE" | head -n 1 | xargs printf)"
 MODULE_DATA_PATH="$DATA_PATH/$(grep_prop id "$MODPATH/module.prop")"
+
+ui_print "- Initialize module data directory"
 
 mkdir -p "$MODULE_DATA_PATH"
 
